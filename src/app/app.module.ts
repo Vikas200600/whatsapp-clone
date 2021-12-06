eea32898bd3b3aefeb00a60c3250dd375241bb54
Vikas M <vikas.m@zysk.in>
2021-12-06 11:40:59 +0530

    list & chat area added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListAreaComponent } from './list-area/list-area.component';
+import { ChatAreaComponent } from './chat-area/chat-area.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ListAreaComponent, ChatAreaComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
