87fe23874af49bc46aac5e179cc4978261c86180
Vikas M <vikas.m@zysk.in>
2021-12-11 18:41:14 +0530

    chat area modify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,23 @@ import { DataService } from './services/data.service';
 export class AppComponent implements OnInit {
   chatData: object;
   userName: string;
-  user: object;
 
   constructor(private dataService: DataService) {
+    console.log('constructor called');
     this.dataService.subject.subscribe((data) => {
       this.chatData = data;
     });
+    this.userName = this.chatData['keys'][0];
+    console.log('constructor userName', this.userName);
+    console.log('constructor userChat', this.chatData[this.userName]);
   }
 
   ngOnInit(): void {
-    this.updateChatWindow(Object.keys(this.chatData)[0]);
+    console.log('ngOnInit userName', this.userName);
+    console.log('ngOnInit called');
   }
 
   updateChatWindow(key: string) {
     this.userName = key;
-    this.user = this.chatData[key];
   }
 }
