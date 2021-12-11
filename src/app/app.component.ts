6dd2aabf7da7ec0de0e74d492ec6e3b8f7620a07
Vikas M <vikas.m@zysk.in>
2021-12-11 20:26:26 +0530

    left-outs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,13 @@ export class AppComponent implements OnInit {
   userName: string;
 
   constructor(private dataService: DataService) {
-    console.log('constructor called');
     this.dataService.subject.subscribe((data) => {
       this.chatData = data;
     });
     this.userName = this.chatData['keys'][0];
-    console.log('constructor userName', this.userName);
-    console.log('constructor userChat', this.chatData[this.userName]);
   }
 
-  ngOnInit(): void {
-    console.log('ngOnInit userName', this.userName);
-    console.log('ngOnInit called');
-  }
+  ngOnInit(): void {}
 
   updateChatWindow(key: string) {
     this.userName = key;
